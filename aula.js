abb43d41a132a612f6b856cd9c2c74e92b97896f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:48:48 -0300

    Naboo

--- aula.js
+++ aula.js
@@ -1,1 +1,22 @@
+//Criando um alerta em JavaScript
 alert("Olá Mundo");
+
+/* 
+
+     ***********************************************
+
+                Adicionando JavaScrit
+       Cirando um comentário com mais de 1 linha
+
+     ***********************************************
+*/
+
+//Adicionando uma mensagemna Tela
+document.write('Naboo')
+
+//Adicionando console.log
+console.log('Que a Força Esteja com Você')
+
+let menssagem= "Olá, Galáxia";
+console.log("Mensagem de Variável: ",menssagem)
+
