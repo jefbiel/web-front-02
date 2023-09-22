bc76825840478f0b1894835d1fe903afeff2d670
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:13:01 -0300

    Tatooine

---
+++ aula3.js
@@ -0,0 +1,19 @@
+//cria uma variável com valor indefinido
+
+let nome;
+
+//cria uma variável vazia
+let disciplina = null;
+
+//cria variável com valor
+let texto = "Exemplo";
+
+console.log(nome);
+
+ 
+
+//git status 
+//gite add
+//git commit -m "Primeiro Commit"
+//git status
+//git push origin_branch
