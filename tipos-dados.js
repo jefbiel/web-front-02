0bbda4cd6d1764f3994dbf87c81cb205fe2f4330
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:33:25 -0300

    Scarif

---
+++ tipos-dados.js
@@ -0,0 +1,18 @@
+//números inteiros
+let idadeAluno = 18; 
+
+//Números flutuantes
+let valorProduto = 5.35;
+
+//string
+let nomeAluno = 'Anakin Skywalker';
+
+//Boolean
+let casado = true;
+
+// console.log(typeof idadeAluno)
+
+// console.log(typeof idadeAluno);
+// console.log(typeof valorProduto);
+// console.log(typeof nomeAluno);
+console.log(casado);
